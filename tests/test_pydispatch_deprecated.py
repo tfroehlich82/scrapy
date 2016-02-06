3fe888561da040a09d6f2ad2da03e14cc6d75d79
Elias Dorneles <eliasdorneles@gmail.com>
2016-02-06 00:08:59 -0200

    add deprecation for pydispatch

---
+++ tests/test_pydispatch_deprecated.py
@@ -0,0 +1,9 @@
+import unittest
+import warnings
+
+
+class DeprecatedPydispatchTest(unittest.TestCase):
+    def test_import_xlib_pydispatch_show_warning(self):
+        with warnings.catch_warnings(record=True) as w:
+            from scrapy.xlib.pydispatch import dispatcher  # NOQA
+            self.assertIn('Importing from scrapy.xlib.pydispatch is deprecated', str(w[0].message))
